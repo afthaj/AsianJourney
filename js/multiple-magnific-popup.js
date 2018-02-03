ed164ec7728ab47429e9fa806136e7a05adfcf3f
AfthaJ <aftha.jaldin88+github@gmail.com>
2018-02-03 18:28:56 +0530

    2018-02-03-1830
    added multiple slideshows for the different destinations.
    added new attractions (sun & surf, history & culture etc)

---
+++ js/multiple-magnific-popup.js
@@ -0,0 +1,13 @@
+(function($){
+  
+  $('.multiple-gallery').each(function() { // the containers for all your galleries
+      $(this).magnificPopup({
+          delegate: 'a', // the selector for gallery item
+          type: 'image',
+          gallery: {
+            enabled:true
+          }
+      });
+  });
+  
+})(jQuery);
